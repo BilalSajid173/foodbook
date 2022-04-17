6cb23c2d5c423289d0aa65c2c3f4ff2e1568d1e0
Bilal <bsajid173@gmail.com>
2022-04-17 20:04:36 +0530

    Add Navbar

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req,res) => {
-    res.send("<h1>Hello there!!! </h1>")
+app.get("/", (req, res) => {
+    res.render("home")
 })
 
+app.get("/add-recipe", (req, res) => {
+    res.render("addrecipe")
+})
 app.listen(3000, (err) => {
     if (!err) {
         console.log("Server running on port 3000")
